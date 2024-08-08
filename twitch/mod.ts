ea4fa7ea03d35aa1e2c3dffc51df788398d97520
Antony Lee <antony@antonylee.co.uk>
2024-08-08 11:02:16 +0100

    added more exports

--- twitch/mod.ts
+++ twitch/mod.ts
@@ -1,3 +1,8 @@
+export type { AuthProvider } from "./auth/auth_provider.ts"
+export type { UserTokens } from "./auth/user_tokens.ts"
+
+export { RefreshingAuthProvider } from "./auth/refreshing_auth_provider.ts"
+export type { RefreshingAuthProviderConfig } from "./auth/refreshing_auth_provider_config.ts"
 export type { RefreshingAuthProviderStore } from "./auth/refreshing_auth_provider_store.ts"
 export { RefreshingAuthProviderFilesystemStore } from "./auth/refreshing_auth_provider_filesystem_store.ts"
-export type { UserTokens } from "./auth/user_tokens.ts"
+export type { RefreshingAuthProviderFilesystemStoreConfig } from "./auth/refreshing_auth_provider_filesystem_store_config.ts"
