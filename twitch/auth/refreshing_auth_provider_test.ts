8ccf118b5eab9ffe8c450f59666cb8d4c33d8be5
Antony Lee <antony@antonylee.co.uk>
2024-08-09 14:18:12 +0100

    test updated to pass
    still need to mock api responses

--- twitch/auth/refreshing_auth_provider_test.ts
+++ twitch/auth/refreshing_auth_provider_test.ts
@@ -2,13 +2,17 @@ import type { AuthProvider } from "./auth_provider.ts"
 import { RefreshingAuthProvider } from "./refreshing_auth_provider.ts"
 import { RefreshingAuthProviderFilesystemStore } from "./refreshing_auth_provider_filesystem_store.ts"
 
+// TODO: to write a test for the exchange code we need to mock the endpoint
+
 Deno.test("Create valid provider with valid store and attempt to get a valid token", () => {
   const storePath: string = "./twitch/test/auth_store"
   const userId: string = "valid"
   const refreshingAuthProviderStore = new RefreshingAuthProviderFilesystemStore({
     storePath,
   })
-  const authProvider: AuthProvider = new RefreshingAuthProvider({
+  const authProvider = new RefreshingAuthProvider({
+    clientId: "xxxxxxxxxx",
+    clientSecret: "xxxxxxxxxx",
     store: refreshingAuthProviderStore,
   })
   authProvider.getTokensByUserId(userId)
