dd407603ac7517d0d2fe09306aaa21a1c5e6c314
Antony Lee <antony@antonylee.co.uk>
2024-08-09 14:16:50 +0100

    added more config options
    * clientId - req
    * clientSecret - req
    * redirectURI - opt

--- twitch/auth/refreshing_auth_provider_config.ts
+++ twitch/auth/refreshing_auth_provider_config.ts
@@ -1,5 +1,8 @@
 import type { RefreshingAuthProviderStore } from "./refreshing_auth_provider_store.ts"
 
 export interface RefreshingAuthProviderConfig {
+  clientId: string
+  clientSecret: string
   store: RefreshingAuthProviderStore
+  redirectURI?: string
 }
