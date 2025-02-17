d40ef6c6caaad1d9612fd5341fc978e986c44d96
Antony Lee <antony@antonylee.co.uk>
2025-02-17 19:06:33 +0000

    formatted

--- packages/twitch/mod_test.ts
+++ packages/twitch/mod_test.ts
@@ -1,6 +1,6 @@
-import { assertEquals } from "@std/assert";
-import { add } from "./mod.ts";
+import { assertEquals } from "@std/assert"
+import { add } from "./mod.ts"
 
 Deno.test(function addTest() {
-  assertEquals(add(2, 3), 5);
-});
+  assertEquals(add(2, 3), 5)
+})
