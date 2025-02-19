666d0d5ba358b32c594bf85b5c096b50c7f8a833
Antony Lee <antony@antonylee.co.uk>
2025-02-19 14:09:20 +0000

    start up example http server

--- packages/twitch/token_generator.ts
+++ packages/twitch/token_generator.ts
@@ -1,23 +1,56 @@
-import { load } from "jsr:@std/dotenv"
+import { load as loadEnvVariables } from "jsr:@std/dotenv"
 
-await load({
-  envPath: `${import.meta.dirname}/.env`,
-  export: true,
-})
+type envVariables = {
+  httpServerPort: number
+  twitchClientId: string
+  twitchClientSecret: string
+}
 
-const twitchClientId: string | undefined = Deno.env.get("TWITCH_CLIENT_ID")
-const twitchClientSecret: string | undefined = Deno.env.get("TWITCH_CLIENT_SECRET")
-
-const validateEnvVariables = (): boolean => {
+const processEnvVariables = async (): Promise<envVariables> => {
+  // Load env variables
+  await loadEnvVariables({
+    envPath: `${import.meta.dirname}/.env`,
+    export: true,
+  })
+  // Get variables from enviroment
+  const httpServerPort: number = parseInt(Deno.env.get("HTTP_SERVER_PORT") ?? "")
+  const twitchClientId: string | undefined = Deno.env.get("TWITCH_CLIENT_ID")
+  const twitchClientSecret: string | undefined = Deno.env.get("TWITCH_CLIENT_SECRET")
+  // Validation
+  if (!httpServerPort) {
+    throw new Error(`HTTP_SERVER_PORT is not set`)
+  }
+  const minHttpServerPort: number = 1000
+  const maxHttpServerPort: number = 9000
+  if (httpServerPort < minHttpServerPort || httpServerPort > maxHttpServerPort) {
+    throw new Error(
+      `HTTP_SERVER_PORT is not within a valid range (${minHttpServerPort} to ${maxHttpServerPort}): ${httpServerPort}`,
+    )
+  }
   if (!twitchClientId) {
     throw new Error(`TWITCH_CLIENT_ID is not set`)
   }
   if (!twitchClientSecret) {
     throw new Error("TWITCH_CLIENT_SECRET is not set")
   }
-  return true
+  // All ok, return them
+  return {
+    httpServerPort,
+    twitchClientId,
+    twitchClientSecret,
+  }
 }
 
-validateEnvVariables()
+const config: envVariables = await processEnvVariables()
 
-console.log(twitchClientId)
+const _httpServer = Deno.serve({
+  port: config.httpServerPort,
+  hostname: "0.0.0.0",
+  handler: (request: Request) => {
+    console.log(request)
+    return new Response("Hello, world")
+  },
+  onListen({ port, hostname }) {
+    console.log(`Server started at http://${hostname}:${port}`)
+  },
+})
